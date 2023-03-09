6b921cc06abece0e67561b6bb3013d3732c9b8a6
RyuuseiOne <3264958746@qq.com>
2023-03-09 11:52:43 +0800

    初次提交

---
+++ src/main/java/com/er/Application.java
@@ -0,0 +1,17 @@
+package com.er;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Ryuusei
+ * @E-mail: 3264958746@QQ.com's:3264958746@qq.com
+ * @Version: 1.0.0
+ * @CreateDate: 2023/3/9 10:34
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+}
